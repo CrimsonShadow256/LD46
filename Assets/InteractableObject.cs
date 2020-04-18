d89a1fd06c718b7c83a1cf3c297f57b1d1ed49d8
CrimsonShadow26 <joel.m.gerber@gmail.com>
2020-04-18 01:01:06 -0500

    Basic Interaction for moving blocks.

---
+++ Assets/InteractableObject.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+	public bool isSelected;
+
+	//When the mouse hovers over the GameObject, it turns to this color (red)
+	Color m_MouseOverColor = Color.red;
+
+	//This stores the GameObject’s original color
+	Color m_OriginalColor;
+
+	//Get the GameObject’s mesh renderer to access the GameObject’s material and color
+	MeshRenderer m_Renderer;
+
+	void Start()
+	{
+		//Fetch the mesh renderer component from the GameObject
+		m_Renderer = GetComponentInChildren<MeshRenderer>();
+		//Fetch the original color of the GameObject
+		m_OriginalColor = m_Renderer.material.color;
+	}
+
+	void OnMouseOver()
+	{
+		// Change the color of the GameObject to red when the mouse is over GameObject
+		m_Renderer.material.color = m_MouseOverColor;
+		isSelected = true;
+	}
+
+	void OnMouseExit()
+	{
+		// Reset the color of the GameObject back to normal
+		m_Renderer.material.color = m_OriginalColor;
+		isSelected = false;
+	}
+}
---
+++ Assets/MoveableObject.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(InteractableObject))]
+[RequireComponent(typeof(Rigidbody))]
+
+public class MoveableObject : MonoBehaviour {
+
+	public 	float				maxMoveSpeed;
+
+	private InteractableObject	myInteraction;
+	private Rigidbody			myRigidBody;
+	private bool 				playerControlled;
+
+	// Use this for initialization
+	void Start () {
+		myInteraction = GetComponent<InteractableObject> ();
+		myRigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			if (myInteraction.isSelected == true)
+				SetPlayerControl ();
+		}
+
+		if (playerControlled) {
+			if (Input.GetMouseButtonUp (0))
+				ReleasePlayerControl ();
+
+			Vector3 newPosition = transform.position;
+			newPosition.z += Time.deltaTime * maxMoveSpeed * Input.GetAxis ("Mouse X");
+			newPosition.y += Time.deltaTime * maxMoveSpeed * Input.GetAxis ("Mouse Y");
+			transform.position = newPosition;
+
+		}
+	}
+
+	void SetPlayerControl(){
+		playerControlled = true;
+		myRigidBody.isKinematic = true;
+	}
+
+	void ReleasePlayerControl(){
+		playerControlled = false;
+		myRigidBody.isKinematic = false;
+	}
+
+}
