704556951318d4b7de374620deb9f7822f9302da
CrimsonShadow26 <joel.m.gerber@gmail.com>
2020-04-18 15:26:48 -0500

    Bob Walking States

---
+++ Assets/Scripts/BobController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobController : MonoBehaviour {
+
+	public float wallCheckRayLength;
+	public float maxWalkSpeed;
+
+
+	private BobStates state;
+	private Rigidbody myRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody> ();
+		state = BobStates.IDLE;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		switch(state){
+		case BobStates.WALK_FORWARD:
+			WalkForward ();
+			break;
+		case BobStates.WALK_BACKWARDS:
+			WalkBackwards ();
+			break;
+		}
+	}
+
+	void WalkForward(){
+		Vector3 newVelocity = myRigidBody.velocity;
+		if (newVelocity.z < maxWalkSpeed)
+			newVelocity.z = maxWalkSpeed;
+		myRigidBody.velocity = newVelocity;
+
+		RaycastHit hit;
+		// Does the ray intersect any objects excluding the player layer
+		if (Physics.Raycast (transform.position + Vector3.up, transform.TransformDirection (Vector3.forward), out hit, wallCheckRayLength)) {
+			Debug.DrawRay (transform.position + Vector3.up, transform.TransformDirection (Vector3.forward) * hit.distance, Color.red);
+			state = BobStates.WALK_BACKWARDS;
+		} else {
+			Debug.DrawRay(transform.position+Vector3.up, transform.TransformDirection(Vector3.forward) * wallCheckRayLength, Color.green);
+		}
+	}
+
+	void WalkBackwards(){
+		Vector3 newVelocity = myRigidBody.velocity;
+		if (newVelocity.z > -maxWalkSpeed)
+			newVelocity.z = -maxWalkSpeed;
+		myRigidBody.velocity = newVelocity;
+
+		RaycastHit hit;
+		// Does the ray intersect any objects excluding the player layer
+		if (Physics.Raycast(transform.position+Vector3.up, transform.TransformDirection(Vector3.back), out hit, wallCheckRayLength))
+		{
+			Debug.DrawRay(transform.position+Vector3.up, transform.TransformDirection(Vector3.back) * hit.distance, Color.red);
+			state = BobStates.WALK_FORWARD;
+		}else {
+			Debug.DrawRay(transform.position+Vector3.up, transform.TransformDirection(Vector3.back) * wallCheckRayLength, Color.green);
+		}
+	}
+
+	public void Clicked(){
+		if (state == BobStates.IDLE)
+			state = BobStates.WALK_FORWARD;
+	}
+
+	enum BobStates {IDLE, WALK_FORWARD, WALK_BACKWARDS, JUMPING, DIEING}
+}
---
+++ Assets/Scripts/ClickableObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(InteractableObject))]
+public class ClickableObject : MonoBehaviour {
+
+	public UnityEvent clickEvent;
+
+	private InteractableObject	myInteraction;
+
+	// Use this for initialization
+	void Start () {
+		myInteraction = GetComponent<InteractableObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			if (myInteraction.isSelected == true)
+				clickEvent.Invoke ();
+		}
+	}
+}
