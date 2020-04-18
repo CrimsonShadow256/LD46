cc8e120bbb7d65ce67ebedf205962a05a41b611a
CrimsonShadow26 <joel.m.gerber@gmail.com>
2020-04-18 16:06:40 -0500

    Bob Jump state

--- Assets/Scripts/BobController.cs
+++ Assets/Scripts/BobController.cs
@@ -7,9 +7,17 @@ public class BobController : MonoBehaviour {
 	public float wallCheckRayLength;
 	public float maxWalkSpeed;
 
+	public float groundCheckRayLength;
+	public float jumpVelocity;
+	public float jumpSuccessCheckDeltaY;
 
 	private BobStates state;
+	private BobStates lastState;
 	private Rigidbody myRigidBody;
+
+	// Jumping variables
+	private bool isJumping = false;
+	private float jumpStartY;
 
 	// Use this for initialization
 	void Start () {
@@ -26,6 +34,9 @@ public class BobController : MonoBehaviour {
 		case BobStates.WALK_BACKWARDS:
 			WalkBackwards ();
 			break;
+		case BobStates.JUMPING:
+			Jumping ();
+			break;
 		}
 	}
 
@@ -36,10 +47,9 @@ public class BobController : MonoBehaviour {
 		myRigidBody.velocity = newVelocity;
 
 		RaycastHit hit;
-		// Does the ray intersect any objects excluding the player layer
 		if (Physics.Raycast (transform.position + Vector3.up, transform.TransformDirection (Vector3.forward), out hit, wallCheckRayLength)) {
 			Debug.DrawRay (transform.position + Vector3.up, transform.TransformDirection (Vector3.forward) * hit.distance, Color.red);
-			state = BobStates.WALK_BACKWARDS;
+			ChangeState (BobStates.JUMPING);
 		} else {
 			Debug.DrawRay(transform.position+Vector3.up, transform.TransformDirection(Vector3.forward) * wallCheckRayLength, Color.green);
 		}
@@ -52,19 +62,57 @@ public class BobController : MonoBehaviour {
 		myRigidBody.velocity = newVelocity;
 
 		RaycastHit hit;
-		// Does the ray intersect any objects excluding the player layer
 		if (Physics.Raycast(transform.position+Vector3.up, transform.TransformDirection(Vector3.back), out hit, wallCheckRayLength))
 		{
 			Debug.DrawRay(transform.position+Vector3.up, transform.TransformDirection(Vector3.back) * hit.distance, Color.red);
-			state = BobStates.WALK_FORWARD;
+			ChangeState (BobStates.JUMPING);
 		}else {
 			Debug.DrawRay(transform.position+Vector3.up, transform.TransformDirection(Vector3.back) * wallCheckRayLength, Color.green);
 		}
 	}
 
+	void Jumping(){
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position+Vector3.up, transform.TransformDirection(Vector3.down), out hit, groundCheckRayLength))
+		{
+			if (isJumping == false) {
+				Debug.DrawRay (transform.position + Vector3.up, transform.TransformDirection (Vector3.down) * hit.distance, Color.green);
+
+				// Add Jump Velocity
+				Vector3 newVelocity = myRigidBody.velocity;
+				newVelocity.y = jumpVelocity;
+				myRigidBody.velocity = newVelocity;
+
+				// Set Jumping Variables
+				isJumping = true;
+				jumpStartY = transform.position.y;
+			} else {
+				isJumping = false;
+
+				if (Mathf.Abs (jumpStartY - transform.position.y) >= jumpSuccessCheckDeltaY) {
+					// Jump success
+					ChangeState (lastState);
+				} else {
+					// Jump failed
+					if (lastState == BobStates.WALK_BACKWARDS)
+						ChangeState (BobStates.WALK_FORWARD);
+					else
+						ChangeState (BobStates.WALK_BACKWARDS);
+				}
+			}
+		}else {
+			Debug.DrawRay(transform.position+Vector3.up, transform.TransformDirection(Vector3.down) * groundCheckRayLength, Color.red);
+		}
+	}
+
+	void ChangeState(BobStates newState){
+		lastState = state;
+		state = newState;
+	}
+
 	public void Clicked(){
 		if (state == BobStates.IDLE)
-			state = BobStates.WALK_FORWARD;
+			ChangeState(BobStates.WALK_FORWARD);
 	}
 
 	enum BobStates {IDLE, WALK_FORWARD, WALK_BACKWARDS, JUMPING, DIEING}
