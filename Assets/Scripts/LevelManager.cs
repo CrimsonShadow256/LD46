91ec968fca392f1734cba5ed1dd08ea3812e296c
CrimsonShadow26 <joel.m.gerber@gmail.com>
2020-04-19 14:25:13 -0500

    Tutorial Levels 1, 2, and 3. Created Scene Manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	[HideInInspector]
+	public LevelManager singelton;
+
+	private int sceneToLoad;
+
+	// Use this for initialization
+	void Start () {
+		if (singelton == null)
+			singelton = this;
+		else
+			Destroy (this);
+	}
+	
+	public void LoadNextLevel(){
+		sceneToLoad = SceneManager.GetActiveScene ().buildIndex + 1;
+		StartSceneTransition ();
+	}
+
+	public void LoadLevelByName(string nextLevel){
+		sceneToLoad = SceneManager.GetSceneByName (nextLevel).buildIndex;
+		StartSceneTransition ();
+	}
+
+	private void StartSceneTransition(){
+		Invoke ("LoadScene", 2.0f);
+	}
+
+	private void LoadScene(){
+		SceneManager.LoadScene (sceneToLoad);
+	}
+}
